31673568741e04b1aaf463bc61a5233dfccf07d8
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2026-08-01 13:41:34 -0700

    Add: coin-change - submission-8

---
+++ Data Structures & Algorithms/coin-change/submission-8.py
@@ -0,0 +1,19 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        reachable = [-1] * (amount + 1)
+
+        reachable[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin > i:
+                    continue
+                
+                if reachable[i - coin] == -1:
+                    continue
+                
+                if reachable[i] == -1:
+                    reachable[i] = reachable[i - coin] + 1
+                else:
+                    reachable[i] = min(reachable[i], reachable[i - coin] + 1)
+        return reachable[-1]
