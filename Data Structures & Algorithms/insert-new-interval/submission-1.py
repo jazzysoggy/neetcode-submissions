85ca0c838ccef21468f4f4c94bd1b45516a24350
Henry Cheng <39224097+jazzysoggy@users.noreply.github.com>
2026-08-03 21:26:01 -0700

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        heapq.heapify(intervals)
+
+        heapq.heappush(intervals, newInterval)
+
+        if len(intervals) == 0:
+            return []
+
+        output = []
+
+        output.append(heapq.heappop(intervals))
+
+        while len(intervals) > 0:
+            new_val = heapq.heappop(intervals)
+            print(output, new_val)
+
+            if output[-1][-1] >= new_val[0]:
+                output[-1][-1] = max(output[-1][-1], new_val[1])
+            else:
+                output.append(new_val)
+
+
+        return output
+
+        
